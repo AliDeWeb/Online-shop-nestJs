5e2771f9ede0999c2973619fff5b82fe90ca77c5
AliDeWeb <alimoradi0business@gmail.com>
2024-08-01 09:29:09 -0700

    .env file added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/beauty-house'),
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.DATABASE),
     AuthModule,
   ],
 })
