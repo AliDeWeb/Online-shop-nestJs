068fe88dda13fc59d07948c7986c77f55a09a59b
AliDeWeb <alimoradi0business@gmail.com>
2024-08-01 09:20:28 -0700

    mongodb configs setted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/beauty-house'),
+    AuthModule,
+  ],
 })
 export class AppModule {}
