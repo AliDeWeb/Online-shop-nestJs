46e6dcef7e0765f6cdd4bc95d6bdf5ab2aeaa749
AliDeWeb <alimoradi0business@gmail.com>
2024-08-01 11:24:39 -0700

    token generation logic developed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,24 @@
+// auth.module.ts
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../schemas/user/user.schema';
 import { UserRepository } from './auth.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
