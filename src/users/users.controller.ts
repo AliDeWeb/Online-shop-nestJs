ca39d41639a402919d850894265775fb964d22ed
AliDeWeb <alimoradi0business@gmail.com>
2024-08-02 03:50:31 -0700

    find user by phone number and email logic developed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly UsersService: UsersService) {}
+
+  @Get('get-user-by-phone-number/:phoneNumber')
+  async getUserByPhoneNumber(@Param('phoneNumber') phoneNumber: string) {
+    if (!phoneNumber.match(/^09[0-9]{9}$/))
+      throw new BadRequestException('phone number is not valid');
+
+    const user = await this.UsersService.findUserByPhoneNumber(phoneNumber);
+
+    user['updatedAt'] = undefined;
+    user['password'] = undefined;
+    user['__v'] = undefined;
+
+    return user;
+  }
+
+  @Get('get-user-by-email/:email')
+  async getUserByEmail(@Param('email') email: string) {
+    if (
+      !email.match(
+        /^(?=.{1,256})(?=.{1,64}@.{1,255}$)(?=\S+$)(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()\[\]\\.,;:\s@"]+\.)+[^<>()\[\]\\.,;:\s@"]{2,})$/,
+      )
+    )
+      throw new BadRequestException('email is not valid');
+
+    const user = await this.UsersService.findUserByEmail(email);
+
+    user['updatedAt'] = undefined;
+    user['password'] = undefined;
+    user['__v'] = undefined;
+
+    return user;
+  }
+}
