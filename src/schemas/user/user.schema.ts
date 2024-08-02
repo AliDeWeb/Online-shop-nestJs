d59178c4114a63b7bb3911a7e4a84d961cac8fef
AliDeWeb <alimoradi0business@gmail.com>
2024-08-02 07:13:19 -0700

    user roles are modular now

--- src/schemas/user/user.schema.ts
+++ src/schemas/user/user.schema.ts
@@ -1,13 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 import * as bcrypt from 'bcryptjs';
+import {
+  userRolesEnum,
+  userRolesType,
+} from 'src/utilities/types/userRoles.type';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop({ type: String, default: 'user', enum: ['user', 'admin'] })
-  role: 'user' | 'admin';
+  @Prop({ type: String, default: 'user', enum: userRolesEnum })
+  role: userRolesType;
 
   @Prop({ type: String, required: 'password is required' })
   password: string;
---
+++ src/utilities/types/userRoles.type.ts
@@ -0,0 +1,3 @@
+export type userRolesType = 'admin' | 'user';
+
+export const userRolesEnum: userRolesType[] = ['admin', 'user'];
