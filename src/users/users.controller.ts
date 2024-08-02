c2753c13c8e69ab3afa46fa59d8d593a6ef43f64
AliDeWeb <alimoradi0business@gmail.com>
2024-08-02 11:38:45 -0700

    swagger api doc for users added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,50 @@
-import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ProtectedRouteGuard } from 'src/auth/guard/protectedRoute.guard';
 import { RolesGuard } from 'src/auth/guard/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
+import {
+  ApiBearerAuth,
+  ApiHeader,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Users')
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly UsersService: UsersService) {}
 
   @UseGuards(ProtectedRouteGuard)
   @Get('me')
+  @HttpCode(200)
+  @ApiResponse({
+    status: 200,
+    description: 'response contains user data witch are not sensitive',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'response contains a error message',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'response contains a error message',
+  })
+  @ApiHeader({
+    name: 'authorization',
+    example: 'Bearer {{Token Here}}',
+    description: 'authorization must be like `Bearer {{Token Here}}`',
+    required: true,
+  })
   async getMe(@Request() request) {
     const user = await this.UsersService.findUserById(request.user.id);
 
@@ -23,6 +58,30 @@ export class UsersController {
   @UseGuards(ProtectedRouteGuard, RolesGuard)
   @Roles('admin')
   @Get('get-user-by-phone-number/:phoneNumber')
+  @HttpCode(200)
+  @ApiResponse({
+    status: 200,
+    description: 'response contains user data witch are not sensitive',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'response contains a error message',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'response contains a error message',
+  })
+  @ApiHeader({
+    name: 'authorization',
+    example: 'Bearer {{Token Here}}',
+    description: 'authorization must be like `Bearer {{Token Here}}`',
+    required: true,
+  })
+  @ApiParam({
+    name: 'phoneNumber',
+    required: true,
+    description: 'this parameter should be a valid phone number',
+  })
   async getUserByPhoneNumber(@Param('phoneNumber') phoneNumber: string) {
     const user = await this.UsersService.findUserByPhoneNumber(phoneNumber);
 
@@ -36,6 +95,30 @@ export class UsersController {
   @UseGuards(ProtectedRouteGuard, RolesGuard)
   @Roles('admin')
   @Get('get-user-by-email/:email')
+  @HttpCode(200)
+  @ApiResponse({
+    status: 200,
+    description: 'response contains user data witch are not sensitive',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'response contains a error message',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'response contains a error message',
+  })
+  @ApiHeader({
+    name: 'authorization',
+    example: 'Bearer {{Token Here}}',
+    description: 'authorization must be like `Bearer {{Token Here}}`',
+    required: true,
+  })
+  @ApiParam({
+    name: 'email',
+    required: true,
+    description: 'this parameter should be a valid email',
+  })
   async getUserByEmail(@Param('email') email: string) {
     const user = await this.UsersService.findUserByEmail(email);
 
