2606bcbd7974dcab85fda10005b2be004c221b5e
AliDeWeb <alimoradi0business@gmail.com>
2024-08-02 13:44:08 -0700

    simple product schema added

---
+++ src/schemas/product/product.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ProductDocument = HydratedDocument<Product>;
+
+@Schema()
+export class Product {
+  @Prop({ type: String, required: [true, 'the product must have a title'] })
+  title: string;
+
+  @Prop({
+    type: String,
+    required: [true, 'the product must have description'],
+  })
+  description: string;
+
+  @Prop({
+    type: String,
+    required: [true, 'the product must have a image'],
+  })
+  image: string;
+
+  @Prop({
+    type: Number,
+    required: [true, 'the product must have price'],
+  })
+  price: number;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
